38094410fe36d8d230824d879b8278b89b366079
LucaSubiranaFord <lucasubiranaford@gmail.com>
2017-04-27 14:51:49 -0300

    Add files via upload

--- TP_2_LABORATORIO/main.c
+++ TP_2_LABORATORIO/main.c
@@ -6,7 +6,6 @@
 
 typedef struct
 {
-
     char nombre[30];
     int edad;
     int dni;
@@ -19,19 +18,17 @@ restaurarEstado(ePersona[], int);
 ordenarNombres(ePersona[], int);
 agregarPersona(ePersona[], int);
 confirmarEliminarPersona(ePersona[], int);
+imprimirGraficoEdades(ePersona[], int);
 
 int main()
 {
-    int borrarDni;
-    int j;
+    int borrarDni, i;
     char buffer[200];
     ePersona personas[P];
     char seguir='s';
     int opcion=0;
-    int i;
 
-
-restaurarEstado(personas, P);
+    restaurarEstado(personas, P);
 
     while(seguir=='s')
     {
@@ -46,56 +43,24 @@ restaurarEstado(personas, P);
         switch(opcion)
         {
         case 1:
-
             agregarPersona(personas, P);
-
             break;
         case 2:
-
             confirmarEliminarPersona(personas, P);
-
             break;
         case 3:
-
-        ordenarNombres(personas, P);
-        for(i=0;i<P;i++)
-        {
-            if(personas[i].estado == 1)
-            {
-            mostrarPersona(personas[i]);
-            }
-        }
-
-            break;
-        case 4:
-
+            ordenarNombres(personas, P);
             for(i=0; i<P; i++)
             {
                 if(personas[i].estado == 1)
                 {
-
-                    if(personas[i].edad < 18)
-                    {
-                        printf("p");
-                    }
-
-                    if(personas[i].edad > 19 && personas[i].edad < 35)
-                    {
-                        printf("\tp\t");
-                    }
-
-                    if(personas[i].edad > 35)
-                    {
-                        printf("\t\tp\t\t");
-                    }
-
-                    printf("\n");
+                    mostrarPersona(personas[i]);
                 }
-
             }
 
-            printf("<18\t19>35\t>35");
-
+            break;
+        case 4:
+            imprimirGraficoEdades(personas, P);
             break;
         case 5:
             seguir = 'n';
@@ -128,19 +93,19 @@ ordenarNombres(ePersona personas[], int tam)
     ePersona auxPersona;
 
     for(i=0; i<tam-1; i++)
+    {
+        for(j=i+1; j<tam; j++)
+        {
+            if(strcmp(personas[i].nombre, personas[j].nombre) > 0)
             {
-                for(j=i+1; j<tam; j++)
-                {
-                    if(strcmp(personas[i].nombre, personas[j].nombre) > 0)
-                    {
-                        auxPersona = personas[i];
-                        personas[i] = personas[j];
-                        personas[j] = auxPersona;
-
-                    }
-                }
+                auxPersona = personas[i];
+                personas[i] = personas[j];
+                personas[j] = auxPersona;
 
             }
+        }
+
+    }
 }
 
 
@@ -149,63 +114,102 @@ agregarPersona(ePersona personas[], int tam)
     int i;
     char agregarOtraPersona = 's';
     for(i=0; i<tam; i++)
-            {
+    {
 
-                if(personas[i].estado == 0 && agregarOtraPersona == 's')
-                {
+        if(personas[i].estado == 0 && agregarOtraPersona == 's')
+        {
 
-                    printf("\nIngrese el nombre de la persona: ");
-                    fflush(stdin);
-                    gets(personas[i].nombre);
+            printf("\nIngrese el nombre de la persona: ");
+            fflush(stdin);
+            gets(personas[i].nombre);
 
-                    printf("\nIngrese la edad de la persona: ");
-                    scanf("%d", &personas[i].edad);
+            printf("\nIngrese la edad de la persona: ");
+            scanf("%d", &personas[i].edad);
 
-                    printf("\nIngrese el DNI de la persona: (solo numeros) ");
-                    scanf("%d", &personas[i].dni);
+            printf("\nIngrese el DNI de la persona: (solo numeros) ");
+            scanf("%d", &personas[i].dni);
 
-                    printf("\nAgregado: ");
-                    mostrarPersona(personas[i]);
+            printf("\nAgregado: ");
+            mostrarPersona(personas[i]);
 
-                    personas[i].estado = 1;
+            personas[i].estado = 1;
 
-                    printf("\nDesea agregar otra persona mas en este momento?(s/n)\n");
-                    agregarOtraPersona = getche();
-                }
-            }
+            printf("\nDesea agregar otra persona mas en este momento?(s/n)\n");
+            agregarOtraPersona = getche();
+        }
+    }
 }
 
 confirmarEliminarPersona(ePersona personas[], int tam)
 {
+    int flagConfirmarEliminar = 0;
     int i;
     int borrarDni;
-    char confirmar;
+    char eliminarPersona;
 
     printf("Ingrese el DNI de la persona que quiere eliminar: ");
-            scanf("%d", &borrarDni);
+    scanf("%d", &borrarDni);
 
     for(i=0; i<tam; i++)
+    {
+        if(personas[i].estado == 1 && personas[i].dni == borrarDni)
+        {
+            mostrarPersona(personas[i]);
+            printf("\nSeguro quiere eliminar a esta persona?");
+            eliminarPersona = getche();
+            flagConfirmarEliminar = 1;
+
+            if(eliminarPersona == 's')
             {
-                if(personas[i].estado == 1 && personas[i].dni == borrarDni )
-                {
-                    mostrarPersona(personas[i]);
-                    printf("\nSeguro desea eliminar a esta persona?(s/n)");
-                    confirmar = getche();
-                }
-
-                if(personas[i].estado == 1 && confirmar == 's')
-                {
-                    personas[i].estado = -1;
-                    printf("\nPersona eliminada.");
-                    break;
-                }
-                else if(personas[i].estado == 1 && confirmar == 'n')
-                {
-                    printf("\nSe ha cancelado la operacion.");
-                    break;
-                }
-
-                printf("\nEsa persona no existe!");
+                printf("\nHa sido eliminado!");
+                personas[i].estado = 0;
                 break;
             }
+            else
+            {
+                printf("\nAccion cancelada");
+                break;
+            }
+        }
+
+    }
+
+    if(flagConfirmarEliminar == 0)
+    {
+        printf("No existe esa persona!");
+    }
+
+
 }
+
+imprimirGraficoEdades(ePersona personas[], int tam)
+{
+    int i;
+    for(i=0; i<tam; i++)
+    {
+        if(personas[i].estado == 1)
+        {
+
+            if(personas[i].edad < 18)
+            {
+                printf("*");
+            }
+
+            if(personas[i].edad > 19 && personas[i].edad < 35)
+            {
+                printf("\t*\t");
+            }
+
+            if(personas[i].edad > 35)
+            {
+                printf("\t\t*\t\t");
+            }
+
+            printf("\n");
+        }
+
+    }
+
+    printf("<18\t19>35\t>35");
+}
+
