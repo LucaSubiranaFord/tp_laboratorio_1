69abd95843c38c8d7220524d7505eec4b4f9b131
Luca Subirana <lucasubiranaford@gmail.com>
2017-06-02 10:00:50 -0300

    Add files via upload

---
+++ TP_3_LABORATORIO/funciones.h
@@ -0,0 +1,52 @@
+#ifndef _EMPLOYEE_H
+#define _EMPLOYEE_H
+
+typedef struct{
+    char titulo[20];
+    char genero[20];
+    int duracion;
+    char descripcion[50];
+    int puntaje;
+    char linkImagen[50];
+}eMovie;
+
+
+/** \brief Agrega una pelicula al tipo de dato ArrayList
+ * \param El tipo de dato ArrayList
+ */
+agregarPelicula(ArrayList*);
+
+
+
+/** \brief Elimina una pelicula del ArrayList
+ * \param El tipo de dato ArrayList
+ */
+borrarPelicula(ArrayList*);
+
+
+
+/** \brief Constructor por defecto de la estructura eMovie
+ * \return un puntero a un tipo de dato eMovie
+ */
+
+eMovie* newMovie();
+
+
+
+/** \brief Modifica una pelicula dentro del ArrayList
+ * \param El tipo de dato ArrayList
+ */
+modificarPelicula(ArrayList*);
+
+
+
+/** \brief genera una pagina web donde se mostraran los datos de cada elemento del ArrayList
+ * \param El tipo de dato ArrayList
+ */
+generarPagina(ArrayList*);
+
+
+#endif // _EMPLOYEE_H
+
+
+
---
+++ TP_3_LABORATORIO/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "funciones.h"
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+    ArrayList* lista;
+    eMovie* aux;
+    lista = al_newArrayList();
+    int i;
+
+    while(seguir=='s')
+    {
+        printf("1- Agregar pelicula\n");
+        printf("2- Borrar pelicula\n");
+        printf("3- Modificar pelicula\n");
+        printf("4- Generar pagina web\n");
+        printf("5- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                agregarPelicula(lista);
+                break;
+            case 2:
+                borrarPelicula(lista);
+                break;
+            case 3:
+                modificarPelicula(lista);
+               break;
+               case 4:
+                   generarPagina(lista);
+               break;
+            case 5:
+                seguir = 'n';
+                break;
+        }
+    }
+
+    return 0;
+}
